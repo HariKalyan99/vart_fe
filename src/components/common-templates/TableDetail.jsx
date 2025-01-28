3cd37d00e83ba5bac8610c157b5852ee358b3d71
Harikalyan99 <harikalyan99tronics@gmail.com>
2025-01-28 17:24:30 +0530

    minor changes in terms of role

--- src/components/common-templates/TableDetail.jsx
+++ src/components/common-templates/TableDetail.jsx
@@ -66,7 +66,7 @@ const AnimalTable = ({
     <>
       <TableRow
         key={induvidual?.id}
-        className="bg-saffron border-2 hover:border-b-8 hover:border hover:border-white hover:border-l-0 hover:border-r-0 hover:drop-shadow-2xl"
+        className={`${induvidual?.animalRole === "zookeeper" ? "bg-black" : "bg-saffron"} border-2 hover:border-b-8 hover:border hover:border-white hover:border-l-0 hover:border-r-0 hover:drop-shadow-2xl`}
       >
         <TableCell>
           <span className="text-white text-lg">
